c60bfaa3b16206adc46514940363a9704a74dcc0
duclonggitnore <duclongsew@gmail.com>
2020-11-19 09:21:00 +0700

    geId-getClass-getAttribute

---
+++ app.js
@@ -0,0 +1,19 @@
+//THAM CHIẾU ELEMENT:
+
+//GET: THAM CHIẾU CHO ĐỐI TƯỢNG.
+//SET: THIET1 LẬP LẠI VALUE.
+
+//THAM CHIẾU VỚI ID:
+
+const cartId = document.getElementById('cart');
+console.log(cartId);
+
+//THAM CHIẾU VỚI CLASS:
+
+const cartClass = document.getElementsByClassName('monney');
+console.log(cartClass);
+
+//THAM CHIẾU VỚI ACTRIBUETE: input
+
+const cartInput = document.getElementsByName('name')[0].getAttribute('value');
+console.log(cartInput);
